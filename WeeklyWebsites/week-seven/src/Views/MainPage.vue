08329f764fb6ef6641f8dd36acc24b28c933fecb
CameronHart1 <82789385+CameronHart1@users.noreply.github.com>
2021-09-08 22:56:39 +1000

    More pracs

---
+++ WeeklyWebsites/week-seven/src/Views/MainPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1 style="margin-top: 0">something</h1>
+  <div class="EntryPage" id="bigdiv">
+    <h1>Lots of really important text</h1>
+    <p id="overw">A subheading</p>
+    <ToDoList @clicked="CheckListUpdated">
+       <p>To Do List</p>
+       <template v-slot:HeadingSlot>
+       <h3>last list activity</h3>
+       </template>
+    </ToDoList>
+    <div>
+      <p> {{LastChange}} </p>
+    </div>
+    <TagList></TagList>
+  </div>
+
+  <button
+    class="but"
+    style="height: 200px; margin-left: 40%"
+    onclick=" 
+    var tempp = document.getElementById('bigdiv');
+    if(tempp.style.backgroundColor == 'white')
+     {tempp.style.backgroundColor = 'aquamarine'}
+    else
+     {tempp.style.backgroundColor = 'white'}
+    "
+  >
+    toggle background
+  </button>
+</template>
+
+<script>
+import ToDoList from "../components/ToDoList.vue";
+import TagList from "../components/Searchtags.vue";
+
+export default {
+  name: "MainPage",
+  components: {
+    ToDoList,
+    TagList,
+  },
+  data() {
+    return {
+      LastChange:"None",
+    };
+  },
+  methods: {
+    CheckListUpdated(Message) {
+      this.LastChange = Message;
+    },
+  },
+};
+</script>
+
+<style scoped src= '..\components\stylemainM.css'></style>
---
+++ WeeklyWebsites/week-seven/src/components/Searchtags.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="ListTest">
+    <label>That List</label>
+    <div v-for="tag in plants" :key ="tag.id">
+        <input
+          type ="checkbox"
+          v-model.lazy ="Seltags"
+          :value = tag.text
+          class ="striked"
+        /><label :for=  tag.text >{{tag.text}}</label>
+    </div>
+
+    <p>Checked Tags: {{ Seltags }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchTags",
+  el: '#app',
+  data(){
+      return {
+          plants:[{text: "Flower"},{text: "Shrub"},{text: "Tree"}],
+          Seltags: []
+      }
+  }
+};
+</script>
+
+<style scoped src= '.\stylemainM.css'>
+</style>
