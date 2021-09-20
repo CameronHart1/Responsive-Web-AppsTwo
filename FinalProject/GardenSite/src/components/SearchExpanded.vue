3dcba17ee2f60c42970869a985c9a73af24daa07
CameronHart1 <82789385+CameronHart1@users.noreply.github.com>
2021-09-20 12:12:29 +1000

    Search Overhaul
    Implemented search functionality for Forum Ded, polished the placeholder search text

--- FinalProject/GardenSite/src/components/SearchExpanded.vue
+++ FinalProject/GardenSite/src/components/SearchExpanded.vue
@@ -1,8 +1,20 @@
 <template>
   <!-- the component for the expanded search bar, the paragraphs will be replaced by ceratin filters
 will need a flexbox, with one element bing a form of variable filters that change depending on search type -->
+  <img
+    src="../assets/Images/SearchMag.svg"
+    id="SearchImg"
+    @mousedown="SearchConfirm"
+  />
   <div class="SearchBox" id="SearchBoxId">
-    <input type="text" ref="Search" id="SearchText" placeholder="Search" />
+    <input
+      type="text"
+      ref="Search"
+      id="SearchText"
+      :placeholder="PlaceHold ? PlaceHold : 'Search'"
+      v-model="SearchQuery"
+      v-on:keyup.enter="SearchConfirm"
+    />
     <div id="filters">
       <p>example filter</p>
       <p>example filter</p>
@@ -16,11 +28,32 @@ will need a flexbox, with one element bing a form of variable filters that chang
 </template>
 
 <script>
+import router from "../router/index.js";
+
 export default {
+  props: {
+    PlaceHold: {
+      type: String,
+    },
+  },
+
+  data() {
+    return {
+      SearchQuery: "",
+    };
+  },
   methods: {
     FocusText() {
       //   when unhiden fires focus event
-        this.$refs.Search.focus()
+      this.$refs.Search.focus();
+    },
+    // triggered when search button hit (or on enter) update path query or sends us to the forum page with search as query 
+    SearchConfirm() {
+      if (this.$route.name != "forum" && this.SearchQuery != "") {
+        router.push({ name: "forum", query: { search: this.SearchQuery } });
+      } else if(this.SearchQuery != ""){
+        router.replace({ name: "forum", query: { search: this.SearchQuery } });
+      }
     },
   },
 };
--- FinalProject/GardenSite/src/views/LayerTwo/DedForum.vue
+++ FinalProject/GardenSite/src/views/LayerTwo/DedForum.vue
@@ -4,8 +4,8 @@ import { OnClickOutside } from "@vueuse/components";
 </script>
 
 <template>
-
-<OnClickOutside
+  <OnClickOutside
+      v-on:keyup.escape = "ExSearch = false; SearchTran();"
     @trigger="
       ExSearch = false;
       SearchTran();
@@ -19,22 +19,24 @@ import { OnClickOutside } from "@vueuse/components";
         SearchTran();
       "
     >
-      <SearchExpanded ref="searchBox" v-if="ExSearch"></SearchExpanded>
-      <h2 v-show="!ExSearch">Search</h2>
+      <SearchExpanded ref="searchBox" :PlaceHold="searchText" v-if="ExSearch"></SearchExpanded>
+      <!-- as it was just a header, there was no harm keeping it rendered with v-show-->
+      <h2 v-show="!ExSearch && searchText != ''">{{ searchText }}</h2>
+      <h2 v-show="!ExSearch && searchText == ''">Search</h2>
     </button>
   </OnClickOutside>
 
-    <div style="background-color: #CEB7DE40;">
-      <h3>Posts</h3>
-      <ForumMes
-        v-for="post in this.Posts"
-        :title="post.Title"
-        :text="post.Text"
-        :tags="post.Tags"
-        :key="post.id"
-      >
-      </ForumMes>
-    </div>
+  <div style="background-color: #ceb7de40">
+    <h3>Posts</h3>
+    <ForumMes
+      v-for="post in this.Posts"
+      :title="post.Title"
+      :text="post.Text"
+      :tags="post.Tags"
+      :key="post.id"
+    >
+    </ForumMes>
+  </div>
 </template>
 
 <script>
@@ -46,12 +48,39 @@ export default {
     return {
       ExSearch: false,
       SearchClass: "SearchButton",
-      Posts: this.$store.state.Posts.ForumPosts,
+      searchText: this.$route.query.search || "",
+      searchtags: [],
     };
   },
   components: { SearchExpanded, ForumMes },
 
-   methods: {
+// the expanded search passes values through the URl so i set up a watcher to check when that gets updated
+  watch: {
+    "$route.query.search"() {
+      this.searchText = this.$route.query.search || "";
+      this.ExSearch = false;
+      this.SearchTran();
+    },
+  },
+
+  computed: {
+    // this is the filtering for the search function
+    Posts() {
+      var FilteredPosts = this.$store.state.Posts.ForumPosts;
+      if (this.searchText != "") {
+        FilteredPosts = FilteredPosts.filter(
+          (post) =>
+            post.Title.toLowerCase().indexOf(this.searchText.toLowerCase()) !=
+              -1 ||
+            post.Text.toLowerCase().indexOf(this.searchText.toLowerCase()) != -1
+        );
+      }
+
+      return FilteredPosts;
+    },
+  },
+
+  methods: {
     SearchTran() {
       if (this.ExSearch) {
         this.SearchClass = "EmptySearch";
